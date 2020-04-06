dffe8e907580bb2d464f599848350bac94ed11bf
louloulfx <louloulfx1@hotmail.fr>
2020-04-06 17:06:02 +0200

    1 container par unité effectuant le script

--- script/generate.py
+++ script/generate.py
@@ -2,9 +2,10 @@ import json
 import calendar
 import time
 import random
+import os 
 
+id_unite = [os.getenv('UNITE')]
 starttime = time.time()
-id_unite = [1, 2, 3, 4, 5]
 id_automate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 id_type_automate = ["0", "1", "2", "3", "4", "5",
                     "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
