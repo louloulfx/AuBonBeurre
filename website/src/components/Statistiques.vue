5f15bcbddefdaa580fa2c2fd8e87d4c29d40f646
Elouan <louloulfx1@hotmail.fr>
2020-04-16 16:00:59 +0200

    Chart v1

--- website/src/components/Statistiques.vue
+++ website/src/components/Statistiques.vue
@@ -1,3 +1,112 @@
-<template>
-    <div>Hello</div>
-</template>
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  name: "LineChart",
+  data() {
+    return {
+      datacollection: null,
+      label: [],
+      data: [],
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.fillData();
+    for (let i = 1; i < 61; i++) {
+      this.label.push(i);
+      this.data.push(this.getRandomInt());
+      console.log(this.label[i]);
+    }
+    this.renderChart(this.datacollection, this.options);
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: this.label,
+        datasets: [
+          {
+            label: "Automate 1",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 2",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 3",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 4",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 5",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 6",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 7",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 8",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 9",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+          {
+            label: "Automate 10",
+            backgroundColor: "#f87979",
+            data: this.data,
+          },
+        ],
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+  },
+};
+</script>
