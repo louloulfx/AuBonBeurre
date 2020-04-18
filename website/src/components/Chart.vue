6d9a4f1a90065e957745dc930d3ce6a698c54d8c
Elouan <louloulfx1@hotmail.fr>
2020-04-18 15:36:46 +0200

    Random data / Export to pdf

--- website/src/components/Chart.vue
+++ website/src/components/Chart.vue
@@ -1,14 +1,15 @@
 <script>
-import { Line } from "vue-chartjs";
-
+import { Line, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
 export default {
   extends: Line,
+  mixins: [reactiveProp],
   name: "LineChart",
   props: {
     chartData: {
       type: Object,
-      required: false
-    }
+      required: false,
+    },
   },
   data() {
     return {
@@ -17,93 +18,33 @@ export default {
           yAxes: [
             {
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
               },
               gridLines: {
-                display: true
-              }
-            }
+                display: true,
+              },
+            },
           ],
           xAxes: [
             {
               gridLines: {
-                display: false
-              }
-            }
-          ]
+                display: false,
+              },
+            },
+          ],
         },
         legend: {
-          display: true
+          display: true,
+          position: "top",
+          labels: {},
         },
         responsive: true,
-        maintainAspectRatio: false
-      }
+        maintainAspectRatio: false,
+      },
     };
   },
   mounted() {
     this.renderChart(this.chartData, this.options);
   },
-  methods: {
-    fillData() {
-      this.datacollection = {
-        labels: this.label,
-        datasets: [
-          {
-            label: "Automate 1",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 2",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 3",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 4",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 5",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 6",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 7",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 8",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 9",
-            backgroundColor: "#f87979",
-            data: this.data
-          },
-          {
-            label: "Automate 10",
-            backgroundColor: "#f87979",
-            data: this.data
-          }
-        ]
-      };
-    },
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-    }
-  }
 };
 </script>
