974e84d5249f65a1fd81a9bffa584711b40d4e96
valentingbt <valentin.guibert@ynov.com>
2020-04-25 15:25:49 +0200

    get data by unites

--- script/backend.py
+++ script/backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_cors import CORS
 import mysql.connector
 from mysql.connector import Error
@@ -10,38 +11,69 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["DEBUG"] = True
 
 
-def converter(o):
+def getDate(o):
     if isinstance(o, (datetime.date, datetime.datetime)):
         return o.timestamp()
 
 
-def makeResponse(data):
-    response = app.response_class(
-        response=json.dumps(data, default=converter),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-
-@app.route('/unites', methods=['GET'])
-def get_unites():
+@app.route('/unites/all', methods=['GET'])
+def get_all_unites():
     connection = mysql.connector.connect(host='185.224.137.214',
                                          database='u646551342_devops',
                                          user='u646551342_devops',
                                          password='devops')
     cursor = connection.cursor()
     try:
-            sql = "SELECT * FROM `automates`"
-            cursor.execute(sql)
-            return makeResponse(cursor.fetchall())
+        sql = "SELECT * FROM `automates`"
+        cursor.execute(sql)
+        return (app.response_class(
+            response=json.dumps(cursor.fetchall(), default=getDate),
+            status=200,
+            mimetype='application/json'
+        ))
     finally:
         if cursor != None:
             cursor.close()
         if connection != None:
             connection.close()
 
-    return makeResponse({})
+    return (app.response_class(
+            response=json.dumps({}, default=getDate),
+            status=200,
+            mimetype='application/json'
+            ))
+
+
+@app.route('/unites', methods=['GET'])
+def get_one_unites():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+    connection = mysql.connector.connect(host='185.224.137.214',
+                                         database='u646551342_devops',
+                                         user='u646551342_devops',
+                                         password='devops')
+    cursor = connection.cursor()
+
+    try:
+        query = "SELECT * FROM `automates` WHERE `nb_unite` = %s"
+        cursor.execute(query, (id,))
+        return (app.response_class(
+            response=json.dumps(cursor.fetchall(), default=getDate),
+            status=200,
+            mimetype='application/json'
+        ))
+    finally:
+        if cursor != None:
+            cursor.close()
+        if connection != None:
+            connection.close()
+    return (app.response_class(
+        response=json.dumps({}, default=getDate),
+        status=200,
+        mimetype='application/json'
+    ))
 
 
 @app.route('/', methods=['GET'])
