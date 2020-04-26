5923536feaa8729a93440fe1fb500ea5ca5a55b1
valentingbt <valentin.guibert@ynov.com>
2020-04-26 14:39:16 +0200

    add navbar component

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <navbar></navbar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import navbar from "./components/Navbar";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    navbar
+  }
 };
 </script>
 
